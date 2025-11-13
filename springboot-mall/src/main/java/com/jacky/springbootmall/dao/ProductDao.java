5d7f6cfa534cafa6f0190e76780f53bef5fb9835
jacky2741 <jacky274@tzuchi.org.tw>
2025-11-13 10:31:30 +0800

    商品功能 - 實作查詢商品列表 (基礎實作)

--- springboot-mall/src/main/java/com/jacky/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/jacky/springbootmall/dao/ProductDao.java
@@ -3,7 +3,12 @@ package com.jacky.springbootmall.dao;
 import com.jacky.springbootmall.dto.ProductRequest;
 import com.jacky.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer id);
     Integer createProduct(ProductRequest productRequest);
 
--- springboot-mall/src/main/java/com/jacky/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/jacky/springbootmall/service/ProductService.java
@@ -3,7 +3,12 @@ package com.jacky.springbootmall.service;
 import com.jacky.springbootmall.dto.ProductRequest;
 import com.jacky.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer id);
     Integer createProduct(ProductRequest productRequest);
 
