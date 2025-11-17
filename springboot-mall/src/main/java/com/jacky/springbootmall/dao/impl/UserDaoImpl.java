89f33e9fbd9a14b78734def3cc8d460bac177a39
jacky2741 <jacky274@tzuchi.org.tw>
2025-11-17 13:45:19 +0800

    帳號功能 - 檢查註冊的Email、隱藏返回的密碼

--- springboot-mall/src/main/java/com/jacky/springbootmall/dao/impl/UserDaoImpl.java
+++ springboot-mall/src/main/java/com/jacky/springbootmall/dao/impl/UserDaoImpl.java
@@ -24,19 +24,39 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public User getUserById(Integer userId) {
-        String sql = "select user_id, email, password, create_date, last_modified_date " +
+        String sql = "select user_id, email, password, created_date, last_modified_date " +
                 "FROM user WHERE user_id=:user_id";
 
         Map<String, Object> map = new HashMap<>();
         map.put("user_id", userId);
 
         List<User> userList = namedParameterJdbcTemplate.query(sql, map, new UserRowMapper());
-        return null;
+        if (!userList.isEmpty()) {
+            return userList.get(0);
+        }else {
+            return null;
+        }
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        String sql = "SELECT user_id, email, password, created_date, last_modified_date " + "FROM user WHERE email = :email";
+
+        Map map = new HashMap<>();
+        map.put("email", email);
+
+        List<User> userList = namedParameterJdbcTemplate.query(sql, map, new UserRowMapper());
+
+        if (!userList.isEmpty()) {
+            return userList.get(0);
+        }else {
+            return null;
+        }
     }
 
     @Override
     public Integer createUser(UserRegisterRequest userRegisterRequest) {
-        String sql = "INSERT INTO user (email, password, create_date, last_modified_date) " +
+        String sql = "INSERT INTO user (email, password, created_date, last_modified_date) " +
                 "VALUES (:email, :password, :createdDate, :lastModifiedDate)";
 
         Map<String, Object> map = new HashMap<>();
--- springboot-mall/src/main/java/com/jacky/springbootmall/dto/UserRegisterRequest.java
+++ springboot-mall/src/main/java/com/jacky/springbootmall/dto/UserRegisterRequest.java
@@ -1,10 +1,13 @@
 package com.jacky.springbootmall.dto;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 public class UserRegisterRequest {
     @NotBlank
+    @Email
     private String email;
+
 
     @NotBlank
     private String password;
